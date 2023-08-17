6347cf61546469e4b31316a225c66c43d184e9e5
lordniz <105753295+lordniz@users.noreply.github.com>
2023-08-16 23:11:31 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Portafolio</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#inicio">Inicio</a></li>
+                <li><a href="#proyectos">Proyectos</a></li>
+                <li><a href="#sobre-mi">Sobre Mí</a></li>
+                <li><a href="#servicios">Servicios</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section id="inicio" class="inicio-section">
+        <div class="inicio-content">
+            <div class="inicio-text izq">
+                <p class="small-text">Un gusto saludarte</p>
+                <p class="large-text">Soy Carlos</p>
+            </div>
+            <div class="inicio-text der">
+                <p class="small-text">Actualmente soy un desarrollador Full Stack,</p>
+                <p class="small-text">con conocimiento amplio en los programas de diseño gráfico.</p>
+            </div>
+        </div>
+        <div class="inicio-image">
+            <img src="assets/img/inicio-fondo.png" alt="Imagen">
+        </div>
+    </section>
+
+
+
+
+    <section id="proyectos">
+        <h1>Proyectos</h1>
+        <!-- Contenido de la sección de proyectos -->
+    </section>
+
+    <section id="sobre-mi">
+        <h1>Sobre Mí</h1>
+        <!-- Contenido de la sección "Sobre Mí" -->
+    </section>
+
+    <section id="servicios">
+        <h1>Servicios</h1>
+        <!-- Contenido de la sección de servicios -->
+    </section>
+
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,10 @@
+// Ejemplo de smooth scrolling al hacer clic en los enlaces del menú
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
