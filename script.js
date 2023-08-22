9e29a02044609202d99fc09b6064a0c013766c40
Lniz <crce.aizu@gmail.com>
2023-08-21 23:04:26 -0500

    portafolio_responsivo

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const toggleIcon = document.querySelector('.toggle-icon');
+// const toggleIcon = document.querySelector('.toggle-icon');
 
-toggleIcon.addEventListener('click', () => {
-toggleIcon.classList.toggle('bx-sun');
-document.body.classList.toggle('dark-mode');
-});
+// toggleIcon.addEventListener('click', () => {
+// toggleIcon.classList.toggle('bx-sun');
+// document.body.classList.toggle('dark-mode');
+// });
+window.onscroll = () => {
+    let header = document.querySelector('.header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
+
