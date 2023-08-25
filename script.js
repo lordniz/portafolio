c30ef81633fe01673badccee13f18bcfe5a80567
Lniz <crce.aizu@gmail.com>
2023-08-24 21:28:06 -0500

    portafolio_html_css_js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// menu icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
 // scroll
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -19,6 +27,10 @@ window.onscroll = () => {
     // sticky navbar
     let header = document.querySelector('.header');
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    //retirar menu icon
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
 
 // swiper 
